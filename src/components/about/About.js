9464dc85404d5e802bd06fe67d2718c43c67221c
Vikas <88672035+vikas-nirmal@users.noreply.github.com>
2022-03-27 18:27:38 +0530

    it's better

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,23 +1,29 @@
-import React from 'react'
-import './About.css'
-import imgAbout from '../../img/about.jpg'
+import React from "react";
+import "./About.css";
+import imgAbout from "../../img/about.jpg";
 
 const About = () => {
   return (
-    <div className='about'>
-        <div className="about-left">
-            <div className="about-card bg"></div>
-            <div className="about-card">
-                <img src={imgAbout} alt="about-image" className='about-img' />
-            </div>
+    <div className="about">
+      <div className="about-left">
+        <div className="about-card bg"></div>
+        <div className="about-card">
+          <img src={imgAbout} alt="about-image" className="about-img" />
         </div>
-        <div className="about-right">
-            <h1 className='about-title'>About Me</h1>
-            <p className="about-sub">Hey, I am vikas Nirmal.</p>
-            <p className="about-desc">Recent computer science graduate with passion for developing scalable web applications and working across the full stack. I am looking to join forces with tech companies to continue to grow my skill set while contributing to the positive outcome of making people "richer,smarter, and happier".</p>
-        </div>
+      </div>
+      <div className="about-right">
+        <h1 className="about-title">About Me</h1>
+        <p className="about-sub">Hey, I am vikas Nirmal.</p>
+        <p className="about-desc">
+          Recent computer science graduate with passion for developing scalable
+          web applications and working across the full stack. I am looking to
+          join forces with tech companies to continue to grow my skill set while
+          contributing to the positive outcome of making people "richer,smarter,
+          and happier".
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
 
 const footer = () => {
   return (
     <div className="sectionfooter">
-        <div className="footer">
-            <div className="social">
-                <a href="#"><i className="fab fa-linkedin"></i></a>
-                <a href="#"><i className="fab fa-github"></i></a>
-                <a href="#"><i className="fab fa-instagram"></i></a>
-            </div>
-            <p className='copyright' >
-                Vikas Kumar Nirmal © 2022
-            </p>
-        </div> 
+      <div className="footer">
+        <div className="social">
+          <a href="https://github.com/vikas-nirmal">
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a href="https://www.instagram.com/vikas__kumar321/">
+            <i className="fab fa-github"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/vikas-nirmal-3b7768217/">
+            <i className="fab fa-instagram"></i>
+          </a>
+        </div>
+        <p className="copyright">Vikas Kumar Nirmal © 2022</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default footer
+export default footer;
